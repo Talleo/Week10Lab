3dbcc840bc17c46379aed8fc91fa934315ae55a4
godim <godim@LeosLaptop>
2022-11-10 10:05:13 -0700

    Finished AdminFilter functionality completed

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,32 +11,39 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
 
 public class AdminFilter implements Filter {
-    
-        @Override
+
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
 
-            // code that is executed before the servlet
-            HttpServletRequest httpRequest = (HttpServletRequest)request;
-            HttpSession session = httpRequest.getSession();
-            
+        // code that is executed before the servlet
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+
+        UserDB udb = new UserDB();
+        User user = udb.get((String) session.getAttribute("email"));
+        int roleId = user.getRole().getRoleId();
+
+        if (roleId != 1) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("login");
+            return;
+        } else {
             chain.doFilter(request, response); // execute the servlet
-            
-            // code that is executed after the servlet
-            
-        
+        }
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        
+
     }
 
     @Override
     public void destroy() {
-       
+
     }
 }
